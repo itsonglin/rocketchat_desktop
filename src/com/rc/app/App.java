f76642b27e1e66ac6b52d0f0ec841fc1f15ea762
song <song@song-pc.office>
2017-07-06 17:34:53 +0800

    1.0.4

--- src/com/rc/app/App.java
+++ src/com/rc/app/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        checkTable();
+        //checkTable();
         Launcher launcher = new Launcher();
         launcher.launch();
     }
